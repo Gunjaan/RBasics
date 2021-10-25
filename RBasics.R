0b60b9d40298342e93c88eb0ea93a66a0caed3c4
Gunjan Verma <74871887+Gunjaan@users.noreply.github.com>
2021-10-26 01:47:04 +0530

    Update RBasics.R

--- RBasics.R
+++ RBasics.R
@@ -14,10 +14,10 @@ data <- data[-c(1, 10, 11, 12)]
 head(data)
 
 # Changing column headings
-colnames(data)[5] <- "Hours spent on social media"
-colnames(data)[6] <- "Being the center of attention"
-colnames(data)[7] <- "Working while being observed"
-colnames(data)[8] <- "Talking to a stranger"
+colnames(data)[5] <- "Hours.spent.on.social.media"
+colnames(data)[6] <- "Being.the.center.of.attention"
+colnames(data)[7] <- "Working.while.being.observed"
+colnames(data)[8] <- "Talking.to.a.stranger"
 
 colnames(data)
 head(data)
@@ -27,8 +27,8 @@ femaleData <- data[data$Gender == "Female",] #contains only females
 maleData <- data[data$Gender == "Male",]     #contains only males
 
 # To view the new datasets in console
-View(femaleData) 
-View(maleData) 
+View(femaleData)
+View(maleData)
 
 # Creating a new data set of people below 25y/o.
 below25 <- data[data$Age < 25,]
@@ -36,4 +36,8 @@ below25 <- data[data$Age < 25,]
 below25_a <- data[data$Age < 25, c(1,3,4)] #(contains only column no. 1,3,4)
 # people below 25y/o, without the specified columns in the table.
 below25_b <- data[data$Age < 25, -c(2,4:7)] #(deletes column no. 2,4,5,6,7)
+
+#To create a subset:
+subData1 <- subset(data, Hours.spent.on.social.media == "Less than 2 hours per day")
+subData2 <- subset(data, Gender == "Female" & Hours.spent.on.social.media == "Less than 2 hours per day")
 
