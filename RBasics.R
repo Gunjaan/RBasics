84e5244b047f1343c26d3dceab561199313fdb12
Gunjan Verma <74871887+Gunjaan@users.noreply.github.com>
2021-10-26 01:06:57 +0530

    Update RBasics.R

--- RBasics.R
+++ RBasics.R
@@ -1,23 +1,32 @@
 # Basics of R programming
+
 # Importing our data set.
 data<- read.csv("/Users/gunjan/Documents/socialAnxiety.csv")
+
 # printing top 6 rows of our dataset
 head(data)
+
 # printing column headings
 colnames(data)
+
 # Deleting columns that we don't need
 data <- data[-c(1, 10, 11, 12)]
 head(data)
+
 # Changing column headings
 colnames(data)[5]<- "Hours spent on social media"
 colnames(data)[6]<- "Being the center of attention"
 colnames(data)[7]<- "Working while being observed"
 colnames(data)[8]<- "Talking to a stranger"
-colnames(data)[9]<- "Feeling anxious in public"
+
 colnames(data)
 head(data)
-femaleData<- data[data$Gender == "Female",]
-maleData<- data[data$Gender == "Male",]
-View(data)
-View(femaleData)
-View(maleData)
+
+# Filtering data and creating a new data set.
+femaleData<- data[data$Gender == "Female",] #contains only females
+maleData<- data[data$Gender == "Male",]     #contains only males
+
+#to view the new datasets in console
+View(femaleData) 
+View(maleData) 
+
